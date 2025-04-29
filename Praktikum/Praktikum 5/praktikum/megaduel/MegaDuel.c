4e5961e15529c3bff6acaa1e10ab48ff3a5c2d75
Third <rafi.34hack@gmail.com>
2025-04-29 16:07:39 +0700

    fix: incorrect pointer and include

--- Praktikum/Praktikum 5/praktikum/megaduel/MegaDuel.c
+++ Praktikum/Praktikum 5/praktikum/megaduel/MegaDuel.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#include "MegaDuel.h"
+#include "MegaNimons.h"
+
 
 void runMegaDuel(LabTrack *L){
     while(L->nEff > 1){
-        print("\n== DUEL ROUND ==\n");
+        printf("\n== DUEL ROUND ==\n");
         duelOnce(L->contents[0], L->contents[1]);
 
         MegaNimons winner = resolveDuel(L->contents[0], L->contents[1]);
         if(winner.id == L->contents[0].id){
             MegaNimons temp;
-            deleteAt(&L, indexOf(*L, L->contents[1].id), &temp);
+            deleteAt(L, indexOf(*L, L->contents[1].id), &temp);
+        }else{
+            MegaNimons temp;
+            deleteAt(L, indexOf(*L, L->contents[0].id), &temp);
         }
     }
 
-    printf("\n== The Champion: %s ==\n", L->contents[0]);
+    printf("\n== The Champion: %s ==\n", L->contents[0].name);
 }
-
-/* Output:
- *   <endline>
- *   == DUEL ROUND ==
- *   Duel: Kevzilla (800.0) vs Melatron (780.0)
- *   Winner: Kevzilla
- *   <endline>
- *   == DUEL ROUND ==
- *   Duel: Kevzilla (800.0) vs Bobzilla (850.0)
- *   Winner: Bobzilla
- *   <endline>
- *   == The Champion: Bobzilla ==
- */
